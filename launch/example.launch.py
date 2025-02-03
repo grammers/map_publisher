45c96d35b252ff15a45eb2b428379c5256373f91
grammers <samuel_karlsson@outlook.com>
2025-02-03 14:48:16 +0100

    fixed paths to work good with param and relative as well as absolute

--- launch/example.launch.py
+++ launch/example.launch.py
@@ -3,25 +3,54 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
 def generate_launch_description():
     ld = LaunchDescription()
+    map_file = LaunchConfiguration('map')
 
+    '''
     grid_map = os.path.join(
         get_package_share_directory('map_publisher'),
         'maps/'
         )
+    map_file = os.path.join(
+        get_package_share_directory('map_publisher'),
+        'maps',
+        'map.yaml'
+        )
+    map_dir = LaunchConfiguration(
+        'map',
+        default=os.path.join(
+            get_package_share_directory('turtlebot3_navigation2'),
+            'map',
+            'map.yaml'))
+    '''
+
+    map_file_arg = DeclareLaunchArgument(
+        'map', 
+        default_value=os.path.join(
+            get_package_share_directory('map_publisher'),
+            'maps',
+            'map.yaml'))
             
     node=Node(
         package = 'map_publisher',
         name = 'map_publisher',
         executable = 'map_publisher',
         parameters=[
-            {'path':grid_map},
-            {'map_name':'map'}
+            {'map': map_file}
         ]
     )
 
     #parameters = [grid_map]
 
     ld.add_action(node)
-    return ld
+    #return ld
+    return LaunchDescription([
+        map_file_arg,
+        node
+    ])
--- map_publisher/map_publisher.py
+++ map_publisher/map_publisher.py
@@ -14,62 +14,92 @@ class MapPublisher(Node):
         self.declare_parameters(
             namespace='',
             parameters=[
-                ('path', 'path'),
-                ('map_name', 'map')
+                ('map', 'no path'),
             ])
 
-        with open(self.get_parameter('path').get_parameter_value().string_value + self.get_parameter('map_name').get_parameter_value().string_value + '.yaml', 'r') as f:
+
+
+        self.path = ''
+        self.yaml_file = ''
+        self.pgm_file = ''
+        self.path_name_spliser() 
+
+        #with open(self.get_parameter('path').get_parameter_value().string_value + self.get_parameter('map_name').get_parameter_value().string_value + '.yaml', 'r') as f:
+        with open(self.path + self.yaml_file, 'r') as f:
             self.yaml = yaml.load(f, Loader=yaml.FullLoader)
 
-
-        self.timer = self.create_timer(1.0, self.publish_map)
-        self.map_publisher = self.create_publisher(OccupancyGrid, '/map', 10)
-        
+        self.pgm_path()
         self.grid_map = OccupancyGrid()
-
         self.read_map()
 
+        self.timer = self.create_timer(1.0, self.publish_map)
+        self.map_publisher = self.create_publisher(OccupancyGrid, '/map/t', 10)
+        
+
     def publish_map(self):
-        self.get_logger().info('tick')
+        #self.get_logger().info('tick')
         self.grid_map.header.stamp = self.get_clock().now().to_msg()
         self.map_publisher.publish(self.grid_map)
 
     def read_map(self):
-        self.get_logger().info('read')
+        #self.get_logger().info('read')
 
-        with open(self.get_parameter('path').get_parameter_value().string_value + self.yaml['image'], 'rb') as pgmf:
+        #with open(self.get_parameter('path').get_parameter_value().string_value + self.yaml['image'], 'rb') as pgmf:
+        with open(self.pgm_file, 'rb') as pgmf:
+        #with open(self.path + self.yaml['image'], 'rb') as pgmf:
             map_img = plt.imread(pgmf)
         
         free_thresh = self.yaml['free_thresh']
         occupied_thresh = self.yaml['occupied_thresh']
         col_len = 0
-        for row in map_img:
+        for col in reversed(range(len(map_img))):
             col_len = col_len + 1
             row_len = 0
-            for nr in row:
+            for row in range(len(map_img[0])):
                 row_len = row_len + 1
-                val = nr / 256.0
-                if nr > 255 - 255.0 * free_thresh:
+                if map_img[col][row] > 255 - 255.0 * free_thresh:
+                    #self.grid_map.data.insert(0,0)
                     self.grid_map.data.append(0)
-                elif nr < 255 - 255.0 * occupied_thresh:
+                elif map_img[col][row] < 255 - 255.0 * occupied_thresh:
+                    #self.grid_map.data.insert(0,100)
                     self.grid_map.data.append(100)
                 else:
+                    #self.grid_map.data.insert(0,-1)
                     self.grid_map.data.append(-1)
 
         self.grid_map.info.resolution = self.yaml['resolution']
-        self.grid_map.info.width = row_len
-        self.grid_map.info.height = col_len
+        self.grid_map.info.width =  row_len
+        self.grid_map.info.height =  col_len
         self.grid_map.info.origin.position.x = self.yaml['origin'][0]
         self.grid_map.info.origin.position.y = self.yaml['origin'][1]
         self.grid_map.info.origin.position.z = self.yaml['origin'][2]
         self.grid_map.info.origin.orientation.x = 0.0
         self.grid_map.info.origin.orientation.y = 0.0
-        self.grid_map.info.origin.orientation.z = 0.0
-        self.grid_map.info.origin.orientation.w = 1.0
+        self.grid_map.info.origin.orientation.z = 0.0 #-0.707
+        self.grid_map.info.origin.orientation.w = 1.0 #0.707
+
+        #self.grid_map.data[1] = 50
 
 
         self.grid_map.header.frame_id = 'map'
 
+    def path_name_spliser(self):
+        full_path = self.get_parameter('map').get_parameter_value().string_value
+        spline_path = full_path.split('/')
+        self.path = '/'.join(spline_path[0:-1]) + '/'
+        self.yaml_file = spline_path[-1]
+
+        self.get_logger().info('in path %s' % full_path)
+        self.get_logger().info('path %s' % self.path)
+        self.get_logger().info('yaml %s' % self.yaml_file)
+
+    def pgm_path(self):
+        pgm = self.yaml['image']
+
+        if len(pgm.split('/')) == 1:
+            self.pgm_file = self.path + pgm
+        else:
+            self.pgm_file = pgm
 
         
 
